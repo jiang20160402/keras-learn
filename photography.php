a643ed1c187137e6c467b04140647096189c285e
Jiang <1005827483@qq.com>
2017-08-28 16:37:07 +0800

    stage1

---
+++ photography.php
@@ -0,0 +1,16 @@
+## 查看摄影的一些网站
+Web address for L'oeil de la photographie! http://www.loeildelaphotographie.com
+
+Web address for American Society of Media Photographers! http://www.asmp.org/
+
+Web address for Photo District News: http://www.pdnonline.com
+
+Peter's website: http://www.peterglendinning.com
+
+Mark's website: http://www.markvalentine.us/
+
+Mark's flickrsite: http://www.flickr.com/markvalentine
+
+Michigan State University Department of Art, Art History, & Design:
+
+http://www.art.msu.edu/
